e49b714b07073a887f505c226157a4c27e1929d8
Casin <a.chashchin@ktsstudio.ru>
2017-04-12 20:34:33 +0300

    fix2

--- src/client/app.py
+++ src/client/app.py
@@ -3,26 +3,27 @@ import time
 import threading
 
 
-class App:
-    def __init__(self, ports, delimiter, stop_command):
-        self.port_names = ports.split(delimiter)
-        self.stop_command = stop_command
-        self.command_listener()
-        # self.command_listener_thread = threading.Thread(target=self.command_listener)
-        # self.command_listener_thread = True
-        # self.command_listener_thread.start()
+def command_listener(stop_command):
+    while True:
+        command = sys.stdin.readline().rstrip()
+        if command == stop_command:
+            print(command)
+            sys.exit(0)
 
-    def command_listener(self):
-        while True:
-            command = sys.stdin.readline().rstrip()
-            if command == self.stop_command:
-                print(command)
-                sys.exit(0)
+
+class App:
+    def __init__(self, ports, delimiter):
+        self.port_names = ports.split(delimiter)
 
 
 if __name__ == '__main__':
+    # command_listener_thread = threading.Thread(target=command_listener, args=(sys.argv[3],))
+    # command_listener_thread.daemon = True
+    # command_listener_thread.start()
+
+    command_listener(sys.argv[3])
+
     app = App(
         sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]
+        sys.argv[2]
     )
