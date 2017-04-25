eeb3c0bd701c129e958448d93f61de65b9370970
Casin <a.chashchin@ktsstudio.ru>
2017-04-25 07:00:23 +0300

    try on mac

--- src/network/config_connections.py
+++ src/network/config_connections.py
@@ -1,6 +1,10 @@
-PORT_PREFIX = 'COM'
+# up virtual ports on mac
+# $ socat -d -d pty,raw,echo=0 pty,raw,echo=0
+
+# PORT_PREFIX = 'COM'
+PORT_PREFIX = '/dev/ttys00'
 
 CLIENTS_PORTS = [
-    ['1'],
-    ['2']
+    ['4'],
+    ['5']
 ]
